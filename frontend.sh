b2c90ef69aba3cc35683b55d594e997b09f9ca22
Narendra T <narendratarra935@gmail.com>
2023-12-30 18:54:59 +0530

    frontend

--- frontend.sh
+++ frontend.sh
@@ -1,8 +1,26 @@
-dnf install nginx -y
-cp expense.conf /etc/nginx/default.d/expense.conf
-rm -rf /usr/share/nginx/html/*
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
-systemctl enable nginx
-systemctl restart nginx
+log_file=/tmp/expense.log
+head() {
+    echo -e "\e[36m$1\e[0"
+}
+head "install nginx software"
+dnf install nginx -y &>>log_file
+echo $?
+head "copy expense config file"
+cp expense.conf /etc/nginx/default.d/expense.conf &>>log_file
+echo $?
+head "remove defalut content"
+rm -rf /usr/share/nginx/html/* &>>log_file
+echo $?
+head "download tha application content"
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>log_file
+echo $?
+head "change the directories"
+cd /usr/share/nginx/html &>>log_file
+echo $?
+head "extracting the content"
+unzip /tmp/frontend.zip &>>log_file
+echo $?
+head "enable and restart the application"
+systemctl enable nginx &>>log_file
+systemctl restart nginx &>>log_file
+echo $?
