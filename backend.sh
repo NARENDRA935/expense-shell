8d04f5f89ea7cff7be241dffe01d3f2ea2e26275
Narendra T <narendratarra935@gmail.com>
2023-12-30 17:20:18 +0530

    backend

--- backend.sh
+++ backend.sh
@@ -5,32 +5,46 @@ head() {
 }
 head "disable nodejs"
 dnf module disable nodejs -y &>>log_file
+echo $?
 head "enable nodejs version"
 dnf module enable nodejs:18 -y &>>log_file
+echo $?
 head "install nodejs"
 dnf install nodejs -y &>>log_file
+echo $?
 head "configure the backend services"
 cp backend.service /etc/systemd/system/backend.service
+echo $?
 head "adding application user"
 useradd expense &>>log_file
+echo $?
 head "remove the existing app content"
 rm -rf /app &>>log_file
+echo $?
 head "creating  app directory"
 mkdir /app &>>log_file
+echo $?
 head "download the application content"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>log_file
+echo $?
 head "change the app directory"
 cd /app &>>log_file
+echo $?
 head "extracting the application content"
 unzip /tmp/backend.zip &>>log_file
+echo $?
 head "download the application dependencies"
 npm install &>>log_file
+echo $?
 head "reload the systemD and restart the backend"
 
 systemctl daemon-reload &>>log_file
 systemctl enable backend &>>log_file
 systemctl restart backend &>>log_file
+echo $?
 head "install mysql client"
 dnf install mysql -y &>>log_file
+echo $?
 head "load schema"
 mysql -h mysql-dev.narendrat.online -uroot -p${mysql_passwd} < /app/schema/backend.sql &>>log_file
+echo $?
