ea22d0228457eb5833789055af180255d67a7503
Narendra T <narendratarra935@gmail.com>
2023-12-30 10:41:15 +0530

    creating shell script for backend

--- backend.sh
+++ backend.sh
@@ -1,31 +1,31 @@
-echo -e "\e[32m disable nodejs\e[0m"
+echo -e  "\e[32 disable nodejs\e[0m"
 dnf module disable nodejs -y
-echo -e "\e[32m enable nodejs verson\e[0m"
+echo -e  "\e[32 enable nodejs version\e[0m"
 dnf module enable nodejs:18 -y
-echo -e "\e[32m install nodejs\e[0m"
+echo install nodejs
 dnf install nodejs -y
-echo -e "\e[32m -e "\e[32m configure the backend services\e[0m"
+echo  configure the backend services
 cp backend.service /etc/systemd/system/backend.service
-echo -e "\e[32m adding application user\e[0m"
+echo adding application user
 useradd expense
-echo -e "\e[32m remove the existing app content\e[0m"
+echo remove the existing app content
 rm -rf /app
-echo -e "\e[32m creating a app directory\e[0m"
+echo creating  app directory
 mkdir /app
-echo -e "\e[32m download the application content\e[0m"
+echo download the application content
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
-echo -e "\e[32m change the app directory\e[0m"
+echo change the app directory
 cd /app
-echo -e "\e[32m extracting the application content\e[0m"
+echo extracting the application content
 unzip /tmp/backend.zip
-echo -e "\e[32m downloadthe application dependancies\e[0m"
+echo download the application dependencies
 npm install
-echo -e "\e[32m reload the systemD and restart the backend\e[0m"
+echo reload the systemD and restart the backend
 
 systemctl daemon-reload
 systemctl enable backend
 systemctl restart backend
-echo -e "\e[32m install mysql client\e[0m"
+echo install mysql client
 dnf install mysql -y
-echo -e "\e[32m  load schema\e[0m"
+echo  load schema
 mysql -h mysql-dev.narendrat.online -uroot -pExpenseApp@1 < /app/schema/backend.sql
