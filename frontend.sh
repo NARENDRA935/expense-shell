cbce9e45ad6c3baa85a3179b9b8405cd874d5dc0
Narendra T <narendratarra935@gmail.com>
2024-01-03 17:11:51 +0530

    frontend

--- frontend.sh
+++ frontend.sh
@@ -2,12 +2,28 @@ component=frontend
 source common.sh
 head "install nginx software"
 dnf install nginx -y &>>log_file
-echo $?
+if [ $? -eq 0 ]; then
+    echo "sucess"
+else
+    echo "failure"
+    exit
+fi
 head "copy expense config file"
 cp expense.conf /etc/nginx/default.d/expense.conf &>>log_file
-echo $?
+if [ $? -eq 0 ]; then
+    echo "sucess"
+else
+    echo "failure"
+    exit
+fi
+
 app_prereq "/usr/share/nginx/html"
 head "enable and restart the application"
 systemctl enable nginx &>>log_file
 systemctl restart nginx &>>log_file
-echo $?
+if [ $? -eq 0 ]; then
+    echo "sucess"
+else
+    echo "failure"
+    exit
+fi
