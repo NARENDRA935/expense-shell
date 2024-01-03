1bbd1e578db1036566e7a5311fc95897cf3ff55a
Narendra T <narendratarra935@gmail.com>
2024-01-03 19:01:30 +0530

    frontend

--- backend.sh
+++ backend.sh
@@ -17,8 +17,6 @@ dnf install nodejs -y &>>log_file
 Stat $?
 head "configure the backend services"
 cp backend.service /etc/systemd/system/backend.service
-
-
 Stat $?
 head "adding application user"
 useradd expense &>>log_file
@@ -30,16 +28,11 @@ Stat $?
 
 head "download the application dependencies"
 npm install &>>log_file
-
 Stat $?
-
 head "reload the systemD and restart the backend"
-
 systemctl daemon-reload &>>log_file
 systemctl enable backend &>>log_file
 systemctl restart backend &>>log_file
-
-
 Stat $?
 head "install mysql client"
 dnf install mysql -y &>>log_file
