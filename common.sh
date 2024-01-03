716bd82e7fabefb8ee44a09ed116eac6e77ab7be
Narendra T <narendratarra935@gmail.com>
2024-01-03 17:28:45 +0530

    frontend

--- common.sh
+++ common.sh
@@ -7,17 +7,46 @@ app_prereq(){
   dir=$1
 head "remove the existing app content"
 rm -rf /$1 &>>log_file
-echo $?
+if [ $? -eq 0 ]; then
+    echo "sucess"
+else
+    echo "failure"
+    exit
+fi
 head "creating  app directory"
 mkdir /$1 &>>log_file
-echo $?
+
+if [ $? -eq 0 ]; then
+    echo "sucess"
+else
+    echo "failure"
+    exit
+fi
 head "download the application content"
 curl -o /tmp/${component}.zip https://expense-artifacts.s3.amazonaws.com/${component}.zip &>>log_file
-echo $?
+
+if [ $? -eq 0 ]; then
+    echo "sucess"
+else
+    echo "failure"
+    exit
+fi
 head "change the app directory"
 cd $1 &>>log_file
-echo $?
+
+if [ $? -eq 0 ]; then
+    echo "sucess"
+else
+    echo "failure"
+    exit
+fi
 head "extracting the application content"
 unzip /tmp/${component}.zip &>>log_file
-echo $?
+
+if [ $? -eq 0 ]; then
+    echo "sucess"
+else
+    echo "failure"
+    exit
+fi
 }
